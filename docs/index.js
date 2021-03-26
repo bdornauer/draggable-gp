8c718a47dd884f16b766f028dcb4621d5f03013b
Benedikt Dornauer <2010738480@stud.fh-kufstein.ac.at>
2021-03-26 22:38:13 +0100

    1a) & 1b)

--- docs/index.js
+++ docs/index.js
@@ -4,49 +4,47 @@ function initDragAndDrop() {
     draggableItemsContainer.addEventListener('dragstart', (e) => {
         e.target.classList.add('dragged');
     });
-
     draggableItemsContainer.addEventListener('dragend', (e) => {
         e.target.classList.remove('dragged');
     });
-
     draggableItemsContainer.addEventListener('dragenter', (e) => {
         if (e.target.dataset.index) {
             e.target.classList.add('dragover');
         }
     });
-
     draggableItemsContainer.addEventListener('dragleave', (e) => {
-        if (e.target.dataset.index) { //access data in the data-index set
+        if (e.target.dataset.index) {
             e.target.classList.remove('dragover');
         }
     });
-
     draggableItemsContainer.addEventListener('dragstart', (e) => {
         e.dataTransfer.setData('text/plain', e.target.dataset.index); // data-index
         e.target.classList.add('dragged');
     });
-
+// ...
     draggableItemsContainer.addEventListener('dragover', (e) => {
         e.preventDefault();
     });
 
-
     draggableItemsContainer.addEventListener('drop', (e) => {
         e.preventDefault();
+        e.target.classList.remove('dragover');
+
         const index1 = e.dataTransfer.getData('text/plain');
         const index2 = e.target.dataset.index;
-        console.log(index1);
-        console.log(index2);
-        swap(index1, index2);
+        swap(index1,index2);
         // TODO: HTML-Elemente tauschen
     });
 
     function swap(index1, index2) {
         const e1 = draggableItemsContainer.querySelector(`[data-index="${index1}"]`);
         const e2 = draggableItemsContainer.querySelector(`[data-index="${index2}"]`);
-        //TODO: swap!
 
-        e2.insertAdjacentElement("afterend", e1);
+        const temp = e1.cloneNode(true);
+        temp.classList.remove('dragged');
+        e2.insertAdjacentElement("afterend", temp);
+        e1.insertAdjacentElement("afterend", e2);
+        e1.remove();
     }
 }
 
@@ -78,5 +76,5 @@ function initTouch() {
     });
 }
 
-initTouch();
+initDragAndDrop();
 
